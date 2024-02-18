a8b65c2739d72170d79fe294205a2b6ad261737e
Greg Thomas <greg.d.thomas@bt.com>
2024-02-18 11:42:44 +0000

    Import new project from Spring initialzr

---
+++ src/test/java/com/example/openapiexample/OpenApiExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.openapiexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenApiExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
